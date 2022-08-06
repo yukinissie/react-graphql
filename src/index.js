8e48cc955209a4bb5fcdbdfe4afc95455a7bb70c
yukinissie <bigtree2490@outlook.jp>
2022-08-06 11:42:34 +0900

    Add ApolloProvider

--- src/index.js
+++ src/index.js
@@ -2,30 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000',
   cache: new InMemoryCache(),
 });
 
-client
-  .query({
-    query: gql`
-      query GetUsers {
-        users {
-          id
-          name
-          email
-        }
-      }
-    `,
-  })
-  .then((result) => console.log(result));
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ApolloProvider>
 );
