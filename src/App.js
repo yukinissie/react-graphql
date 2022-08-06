b7fb68a009bb0d2510468beb4926edb2f037cb81
yukinissie <bigtree2490@outlook.jp>
2022-08-06 11:51:48 +0900

    Add users query

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import { gql, useQuery } from '@apollo/client';
+
+const GET_USERS = gql`
+  query GetUsers {
+    users {
+      id
+      name
+      email
+    }
+  }
+`;
+
 function App() {
+  const { data, loading, error } = useQuery(GET_USERS);
+
+  if (loading) return '<p>ローディング中です</p>';
+  if (error) return '<p>エラーが発生しています。<p>';
+
   return (
     <div style={{ margin: '3em' }}>
       <h1>GraphQL</h1>
+      {data.users.map((user) => (
+        <div key={user.id}>Name: {user.name}</div>
+      ))}
     </div>
   );
 }
